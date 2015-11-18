81855e9f41628f5dc9e4f5f89dd14b15b1113522
Bogdan Anton <contact@bogdananton.ro>
2015-11-18 23:20:03 +0200

    Updated self_test (Linux version) to point to Selenium and PHPUnit sh files. Fixed argument list.

--- start_phpunit_tests.sh
+++ start_phpunit_tests.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 echo "Starting tests ..."
-php build/phpunit.phar -c phpunit.xml --testsuite "unix"
+php build/phpunit.phar -c $DIR/phpunit.xml --testsuite "unix"
 
 echo "Stopping previous processes ..."
 pgrep -f 'selenium-server.jar' | xargs kill
--- start_selenium.sh
+++ start_selenium.sh
@@ -1,16 +1,15 @@
 #!/usr/bin/env bash
 
-# $1 selenium.mode
-# $2 selenium.server.host
-# $3 selenium.server.port
-# $4 proxyHost
-# $5 proxyPort
+# $1 selenium.server.host
+# $2 selenium.server.port
+# $3 proxyHost
+# $4 proxyPort
 
-if [ -n "$4" ]
+if [ -n "$3" ]
     then
         echo "Specific proxy given, invalidate system proxies ..."
-        export http_proxy=
-        export https_proxy=
+        export http_proxy=""
+        export https_proxy=""
 fi
 
 echo "Include $PWD build in the global PATH ..."
@@ -18,12 +17,12 @@ echo "Note: this is needed for the web drivers be found by Selenium."
 export PATH=$PATH:$PWD/build
 
 echo "Starting Selenium standalone server ..."
-java -jar build/selenium-server.jar -port $3 -Dhttp.proxyHost="$4" -Dhttp.proxyPort="$5" -Dwebdriver.chrome.driver="./build/chromedriver" -log build/logs/selenium.log > /dev/null &
+java -jar build/selenium-server.jar -port $2 -Dhttp.proxyHost="$3" -Dhttp.proxyPort="$4" -Dwebdriver.chrome.driver="./build/chromedriver" -log build/logs/selenium.log > /dev/null &
 
 echo "Wait ..."
 sleep 5
 
-if [ "$1" -eq "self-test" ]
-    then
-
-fi
+# if [ "$1" -eq "self-test" ]
+#    then
+#
+# fi
