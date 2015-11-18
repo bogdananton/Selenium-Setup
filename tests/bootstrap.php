438f8006a4a7a5ef7db622cdc2872e9abc64bcb2
Serban Ghita <serbanghita@gmail.com>
2015-11-18 20:36:36 +0200

    Pick the right autoloader (in case the build.xml is extended by external project).

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,2 +1,15 @@
 <?php
-require_once dirname(__FILE__) . '/../vendor/autoload.php';
+$composer = dirname(__FILE__) . '/../vendor/autoload.php';
+
+if (!file_exists($composer)) {
+    $composer = dirname(__FILE__) . '/../../../autoload.php';
+}
+
+if (!file_exists($composer)) {
+    throw new \RuntimeException("Please run 'composer install' first to set up autoloading. $composer");
+}
+
+/**
+ * @var \Composer\Autoload\ClassLoader $autoloader
+ */
+$autoloader = include_once $composer;
