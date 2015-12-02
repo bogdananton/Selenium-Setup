70c7907790a32e15fca54cf0859f76426724eb96
Serban Ghita <serbanghita@gmail.com>
2015-12-02 23:01:21 +0200

    CLI fix

--- lib/Command/CommandLinux.php
+++ lib/Command/CommandLinux.php
@@ -36,8 +36,8 @@ class CommandLinux extends CommandWindows
     public function startDisplay()
     {
         $cmd = 'if ! xset q &>/dev/null; then ';
-            $cmd .= 'export DISPLAY=:99 && ';
-            $cmd .= '/sbin/start-stop-daemon --start --pidfile /tmp/custom_xvfb_99.pid --make-pidfile --background --exec /usr/bin/Xvfb -- :99 -ac -screen 0 1280x1024x16; ';
+            $cmd .= 'export DISPLAY=:99.0 && ';
+            $cmd .= '/sbin/start-stop-daemon --start --pidfile /tmp/custom_xvfb_99.pid --make-pidfile --background --exec /usr/bin/Xvfb -- :99 -ac -screen 0 1280x1024x16 && ';
             $cmd .= 'sleep 3; ';
         $cmd .= 'fi;';
 
